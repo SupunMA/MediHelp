6e38a17dcba2081597d381649d5be551bc97d611
SupunMA <supunmadhusanka62@gmail.com>
2023-08-10 11:20:58 +0530

    admin logout modal updated

--- resources/views/layouts/adminComponents/modal/logoutModal.blade.php
+++ resources/views/layouts/adminComponents/modal/logoutModal.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">3 Kings Swimming Pool Complex</h5>
+                <h3 class="modal-title" id="staticBackdropLabel">MediHelp</h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
