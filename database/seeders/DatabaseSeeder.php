a17c52d604a8196f50bcedafeff4f22a89b6bbe4
SupunMA <supunmadhusanka62@gmail.com>
2023-08-12 20:48:55 +0530

    data seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\AvailableTest;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,41 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Jane Doe',
+            'email' => 'admin@a.com',
+            'password' => Hash::make('12345678'),
+	    'role' => 1
+        ]);
+
+        AvailableTest::create([
+            'AvailableTestName' => 'Fasting Blood Sugar',
+            'AvailableTestRange' => '70-120',
+            'AvailableTestCost' => 1100,
+        ]);
+        
+        AvailableTest::create([
+            'AvailableTestName' => 'Full Blood Count',
+            'AvailableTestRange' => '80-180',
+            'AvailableTestCost' => 1000,
+        ]);
+        
+        AvailableTest::create([
+            'AvailableTestName' => 'Lipid Profile',
+            'AvailableTestRange' => '80-180',
+            'AvailableTestCost' => 1200,
+        ]);
+        
+        AvailableTest::create([
+            'AvailableTestName' => 'Microalbumin (Urine)',
+            'AvailableTestRange' => '80-180',
+            'AvailableTestCost' => 1200,
+        ]);
+        
+        AvailableTest::create([
+            'AvailableTestName' => 'Urea & Electrolytes',
+            'AvailableTestRange' => '80-180',
+            'AvailableTestCost' => 800,
+        ]);
     }
 }
