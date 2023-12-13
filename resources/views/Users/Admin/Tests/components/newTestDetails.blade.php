70729ba4354ea70d2d27c0880a731fe440aaecaf
SupunMA <supunmadhusanka62@gmail.com>
2023-12-13 10:09:31 +0530

    date label changed

--- resources/views/Users/Admin/Tests/components/newTestDetails.blade.php
+++ resources/views/Users/Admin/Tests/components/newTestDetails.blade.php
@@ -11,17 +11,17 @@
 <form action="{{ route('admin.addingTest') }}" method="post">
                 @csrf
             <div class="row">
-            
+
                 <div class="col-lg-6 col-12">
                     <div class="form-group">
                         <label>Select The Patient</label>
-                        
+
                         <select class="form-control select2" style="width: 100%;" name="pid">
                             {{-- <option selected="selected">Alabama</option> --}}
                             @foreach ($patients as $patient)
                                 <option value="{{$patient->pid}}">{{$patient->user->name}} - {{$patient->dob}}</option>
                             @endforeach
-                            
+
                         </select>
                     </div>
                 </div>
@@ -31,13 +31,13 @@
                 <div class="col-lg-6 col-12">
                     <div class="form-group">
                         <label>Select The Test</label>
-                        
+
                         <select class="form-control select2" style="width: 100%;" name="tlid">
                             {{-- <option selected="selected">Alabama</option> --}}
                             @foreach ($availableTests as $availableTest)
                                 <option value="{{$availableTest->tlid}}">{{$availableTest->AvailableTestName}} - Rs.{{$availableTest->AvailableTestCost}}</option>
                             @endforeach
-                            
+
                         </select>
                     </div>
                 </div>
@@ -51,7 +51,7 @@
                     <!-- Date dd/mm/yyyy -->
                     <!-- Date -->
                     <div class="form-group">
-                        <label>Date of Birth (M/D/Y)</label>
+                        <label>Date (M/D/Y)</label>
 
                         <div class="input-group date">
                         <div class="input-group-addon">
@@ -67,7 +67,7 @@
                 <div class="col-lg-6 col-12">
                     <div class="form-group">
                         <label>Name of the Doctor</label>
-                        
+
                         <input type="text" name="doctorName" class="form-control" placeholder="Name of the Doctor">
                     </div>
                 </div>
@@ -82,8 +82,8 @@
                     </button>
                 </div>
             </div>
-    
- </form>           
+
+ </form>
         </div>
     </div>
     <!-- /.box -->
